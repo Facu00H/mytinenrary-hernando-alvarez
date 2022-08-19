c233ab8be105dd65ef85d2aefc9e0371f77d9406
Facu00H <faisher00@gmail.com>
2022-08-18 23:27:03 -0300

    Implementacion Hero.js y estilos

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Hero.css'
+
+const Hero = () => {
+  return (
+    <header className="header" Style="">
+      <h1 className="header-title">MyTinerary</h1>
+      <p className="header-p">Viajar se siente bien, Disfruta de los mejores destinos, Tu felicidad es nuestra prioridad Solo disfruta y pásala bien!</p>
+      <button className="header-btn">Ingresar!</button>
+    </header>
+  );
+}
+
+export default Hero;
