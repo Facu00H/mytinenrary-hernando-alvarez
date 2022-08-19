9cd435ff72b853a4b9a2a452fbbbc567c75c8399
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-08-19 16:22:56 -0300

    modified footer's images path

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import '../styles/Footer.css'
-import facebook from '../assets/img/facebook_icon.png'
-import instagram from '../assets/img/instagram_icon.png'
 
 export default function Footer() {
     return (
@@ -14,8 +12,8 @@ export default function Footer() {
                     <h3>MyTinerary</h3>
                 </div>
                 <div className='footer-socialNetworks'>
-                    <a href="https://www.facebook.com/"><img src={facebook} alt=""/></a>
-                    <a href="https://www.instagram.com/"><img src={instagram} alt=""/></a>
+                    <a href="https://www.facebook.com/"><img src="./assets/img/facebook_icon.png"/></a>
+                    <a href="https://www.instagram.com/"><img src="./assets/img/instagram_icon.png"/></a>
                 </div>
             </footer>
         </>
