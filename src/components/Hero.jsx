a2396146b07ed2bbb7c952d9162ed20fd9224ab3
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-08-20 02:21:09 -0300

    changed button tag to anchor tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
   return (
     <header className="header" Style="">
       <h1 className="header-title">MyTinerary</h1>
-      <p className="header-p">Viajar se siente bien, Disfruta de los mejores destinos, Tu felicidad es nuestra prioridad Solo disfruta y pásala bien!</p>
-      <button className="header-btn">Ingresar!</button>
+      <p className="header-p">Viajar se siente bien, disfruta de los mejores destinos, tu felicidad es nuestra prioridad, solo disfruta y pásala bien!</p>
+      <a className="header-btn">Ingresar!</a>
     </header>
   );
 }
