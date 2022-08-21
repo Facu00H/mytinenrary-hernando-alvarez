8cfb128bf8b1ca6b977acbdd6e431d2d5ced04bc
Joaquinbrialva <joaquinbrialva@gmail.com>
2022-08-21 19:54:43 -0300

    added Cities page link to 'ingresar' anchor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <header className="header" Style="">
       <h1 className="header-title">MyTinerary</h1>
       <p className="header-p">Viajar se siente bien, disfruta de los mejores destinos, tu felicidad es nuestra prioridad, solo disfruta y pásala bien!</p>
-      <a className="header-btn">Ingresar!</a>
+      <a href="./pages/Cities.jsx" className="header-btn">Ingresar!</a>
     </header>
   );
 }
