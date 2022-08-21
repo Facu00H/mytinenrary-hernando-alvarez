37db3b04ad04de66717683dc80ed60ce4d975a40
Joaquinbrialva <joaquinbrialva@gmail.com>
2022-08-21 19:56:21 -0300

    Cities page created

---
+++ src/pages/Cities.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WebsiteLayout from "../layouts/WebsiteLayout";
+import Carousel from "../components/Carousel";
+
+function Cities() {
+    return (
+      <div>
+        <Carousel />
+        <WebsiteLayout />
+      </div>
+    );
+  }
+  
+  export default Cities;
