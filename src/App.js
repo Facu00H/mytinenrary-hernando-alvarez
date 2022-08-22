1fe018fb6f41729408f0368ad09fea721f82a320
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-08-22 16:23:27 -0300

    nothing changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import WebsiteLayout from './layouts/WebsiteLayout';
 import Hero from './components/Hero';
+import Carousel from './components/Carousel';
+import WebsiteLayout from './layouts/WebsiteLayout';
 
 function App() {
   return (
     <div>
       <Hero />
+      <Carousel />
       <WebsiteLayout />
     </div>
   );
