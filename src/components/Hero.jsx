9a2f265ef55ce38cada97fe6fe0d6f9fb2034c56
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-08-22 16:17:45 -0300

    translated to English

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
   return (
     <header className="header" Style="">
       <h1 className="header-title">MyTinerary</h1>
-      <p className="header-p">Viajar se siente bien, disfruta de los mejores destinos, tu felicidad es nuestra prioridad, solo disfruta y pásala bien!</p>
-      <a href="./pages/Cities.jsx" className="header-btn">Ingresar!</a>
+      <p className="header-p">Find your perfect trip, designed by insiders who know and love their cities!</p>
+      <a href="./pages/Cities.jsx" className="header-btn">ENTER</a>
     </header>
   );
 }
