771e4c62179e8f90503b4fe0d480ccadf0eea739
Facu00H <faisher00@gmail.com>
2022-08-23 22:40:40 -0300

    remove href, add LinkRouter

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../styles/Hero.css'
+import {Link as LinkRouter} from 'react-router-dom'
+
 
 const Hero = () => {
   return (
     <header className="header" Style="">
       <h1 className="header-title">MyTinerary</h1>
       <p className="header-p">Find your perfect trip, designed by insiders who know and love their cities!</p>
-      <a href="./pages/Cities.jsx" className="header-btn">ENTER</a>
+      <LinkRouter to="/cities" className="header-btn">ENTER</LinkRouter>
     </header>
   );
 }
