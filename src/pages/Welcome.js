6af29e2570ed428e8ce384a89ca74dfaaa5f1c28
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-08-24 18:25:46 -0300

    added footer component

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '.././components/Hero'
 import Carousel from '.././components/Carousel'
+import Footer from '.././components/Footer'
 
 export default function Welcome() {
   return (
     <>
-      <Hero/>
-      <Carousel/>
+        <Hero/>
+        <Carousel/>
+        <Footer/>
     </>
   )
 }
