526db55244c5d865777cf6f262b8d7e61aff0022
Facu00H <faisher00@gmail.com>
2022-08-23 22:21:32 -0300

    Header structure

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/Header.css'
+import Burguer from '../components/Header-components/Burguer'
+
+
+const Header = () => {
+
+  return (
+    <div className="nav">
+      <Burguer/>
+      <h1>MyTinerary</h1>
+      <div className="userImg">
+        <img className="userImg" src='https://icon-library.com/images/generic-user-icon/generic-user-icon-19.jpg' alt="User Profile"/>  
+      </div>
+    </div>
+  );
+}
+
+export default Header;
