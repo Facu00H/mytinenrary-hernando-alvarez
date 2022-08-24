952c3f0ebb9e7aa437e57033d3326eddf1a0f7a1
Facu00H <faisher00@gmail.com>
2022-08-24 13:38:08 -0300

    add layout container

--- src/layouts/WebsiteLayout.jsx
+++ src/layouts/WebsiteLayout.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import '../App.css'
 
 export default function WebsiteLayout(props) {
 
     
 
     return (
-        <>
+        <div className="layout">
             <Header/>
             {props.children}
             <Footer />
-        </>
+        </div>
     )
 }
