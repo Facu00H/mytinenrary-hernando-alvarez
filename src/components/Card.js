8fa121d1b9be25680c35fda73a5a16a404a22c69
Facu00H <faisher00@gmail.com>
2022-08-25 01:40:33 -0300

    generate cards dinamic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,30 @@
 import React from 'react';
+import { items } from '../data/ArrayCiudades';
 import '../styles/Card.css'
 
 const Card = () => {
-  return (
-    <div className="container">
-      <div className="card">
-        <div className="container-img">
-          <img className="card-img" src="https://cdn.pixabay.com/photo/2022/08/11/14/28/woman-7379683__340.jpg" alt="" />
-        </div>
-        <div className="card-content">
-          <h3>asd</h3>
-          <p>asdasd</p>
-          <p>asdasd</p>
-          <div className="btns-container">
-            <div className="btn-card">Contact us!</div>
-            <div className="btn-card">More info</div>
-          </div>
+
+  const card = item => { 
+
+    return (
+    <div className="card" id="asd">
+      <div className="container-img">
+        <img className="card-img" src={item.url} alt="" />
+      </div>
+      <div className="card-content">
+        <h3>{item.title}</h3>
+        <div className="btns-container">
+          <div className="btn-card" id={item.id}>Why Visit it?</div>
+          <div className="btn-card" id={item.id}>Contact us!</div>
         </div>
       </div>
     </div>
+  )} 
+
+  return (
+    <div className="container">
+      {items.map(card)}
+    </div>
   );
 }
-
 export default Card;
