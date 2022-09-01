999e440a873a732f6d5f3f5c050bb998457dd9fa
Facu00H <faisher00@gmail.com>
2022-08-31 23:10:29 -0300

    create details page

---
+++ src/pages/Details.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WebsiteLayout from '.././layouts/WebsiteLayout'
+import UnderConstruction from '.././layouts/UnderConstruction'
+
+
+export default function Details() {
+  return (
+    <WebsiteLayout>
+      <UnderConstruction/>
+    </WebsiteLayout>
+  )
+}
