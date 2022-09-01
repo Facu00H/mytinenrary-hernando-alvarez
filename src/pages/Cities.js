91f55295ef64ac191a58956db7afbffcc3de4c70
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-01 05:45:29 -0300

    no changes

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import WebsiteLayout from "../layouts/WebsiteLayout";
 import Card from '../components/Card'
+import Input from '../components/Input';
 
 function Cities() {
     return (
