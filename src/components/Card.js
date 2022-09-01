054920a524ef02474b96d58ef88304cbb69c089f
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-08-31 23:39:07 -0300

    cardcity render with api

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import { items } from '../data/ArrayCiudades';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import '../styles/Card.css'
 
 const Card = () => {
 
-  const card = item => { 
+  const [cities, setCities] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/cities/')
+      .then(res => setCities(res.data.response))
+      .catch(err => console.error(err))
+  }, [])
+
+  const card = item => {
 
     return (
-    <div className="card" id="asd">
-      <div className="container-img">
-        <img className="card-img" src={item.url} alt="" />
-      </div>
-      <div className="card-content">
-        <h3>{item.title}</h3>
-        <div className="btns-container">
-          <div className="btn-card" id={item.id}>Why Visit it?</div>
-          <div className="btn-card" id={item.id}>Contact us!</div>
+      <div className="card" id="asd">
+        <div className="container-img">
+          <img className="card-img" src={item.photo} alt="" />
+        </div>
+        <div className="card-content">
+          <h3>{item.city}</h3>
+          <div className="btns-container">
+            <div className="btn-card" id={item.id}>Why Visit it?</div>
+            <div className="btn-card" id={item.id}>Contact us!</div>
+          </div>
         </div>
       </div>
-    </div>
-  )} 
+    )
+  }
 
   return (
     <div className="container">
-      {items.map(card)}
+      {cities.map(card)}
     </div>
   );
 }
