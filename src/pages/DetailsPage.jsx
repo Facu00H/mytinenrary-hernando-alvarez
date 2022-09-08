243ef92c86ecd8f48869d568a4e9ed0f12803ba7
Facu00H <faisher00@gmail.com>
2022-09-07 22:13:43 -0300

    add Itinerary component

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import WebsiteLayout from '../layouts/WebsiteLayout'
 import Details from '../components/Details'
 import '../styles/DetailsPage.css'
+import Itinerary from '../components/Itinerary';
 
 export default function DetailsPage() {
 
   return (
     <WebsiteLayout>
       <Details />
+      <Itinerary/>
     </WebsiteLayout>
   )
 }
