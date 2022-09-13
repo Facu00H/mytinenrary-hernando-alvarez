01ddb83bb57f347510bde7496c1bae960d3af1a2
jojope38 <peracchiojoaquin16@gmail.com>
2022-09-13 17:15:37 -0300

    add pages sign and styles

---
+++ src/pages/SignIn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WebsiteLayout from "../layouts/WebsiteLayout";
+import SingInn from '../components/SignIn'
+
+function SignIn() {
+    return (
+      <>
+        <WebsiteLayout>
+          <SingInn/>
+        </WebsiteLayout>
+      </>
+    );
+  }
+  
+  export default SignIn;
