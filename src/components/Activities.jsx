b31c7831e6ac4c70722ca87d0a6d3d405d1a6efb
Joaquin Alvarez <joaquinbrialva@gmail.com>
2022-09-13 17:39:58 -0300

    activities component fix

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,43 +1,24 @@
 import axios from 'axios';
 import React from 'react'
 import { useEffect, useState } from 'react';
-import '../styles/Activities.css'
+import '../styles/Activity.css'
 
-function Activity() {
-
-    const [itinerary, setItinerary] = useState([])
+function Activity(props) {
+    
     const [activities, setActivities] = useState([])
-    const queryString = window.location.search.replace('?', '');
-    let url = `http://localhost:4000/itineraries/city/${queryString}`
 
     useEffect(() => {
-        axios.get(url)
-            .then(res => setItinerary(res.data.response))
+        axios.get(`http://localhost:4000/activities/${props.itinerary}`)
+            .then(res => setActivities(res.data.response))
             .catch(err => console.error(err))
-    }, [])
+    }, [props.itinerary])
 
-
-    useEffect(() => {
-        itinerary.forEach(itinerary => {
-            axios.get(`http://localhost:4000/activities/itinerary/${itinerary._id}`)
-                .then(res => setActivities(res.data.response))
-                .catch(err => console.error(err))
-        })
-    }, [itinerary])
 
     const cardActivities = (data) => {
         return (
-            <div>
-                <div className="container-activities">
-                    <div>
-                        <div>
-                            <img className="activities-img" src={data.photo} alt={data.name} />
-                        </div>
-                        <div className="activities-name">
-                            <p>Name: {data.name}</p>
-                        </div>
-                    </div>
-                </div>
+            <div className='activity-container'>
+                <img className='activities-img' src={data.photo} alt={data.name} />
+                <h5 className='activity-name'>{data.name}</h5>
             </div>
         )
     }
