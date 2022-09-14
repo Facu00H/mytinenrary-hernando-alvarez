12739930b35fdeb655a720ed4ecf69c2b8b1d52f
jojope38 <peracchiojoaquin16@gmail.com>
2022-09-14 18:42:25 -0300

    add alert for SignIn

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,10 @@
 import React, { useEffect,useState } from 'react'
 import {useAddUserSignInMutation} from '../features/citiesAPI'
 import SignInGoogle from './SignInGoogle'
+import Swal from 'sweetalert2'
 export default function SignIn() {
 
-let [email,setEmail]=useState()
+let [mail,setEmail]=useState()
 let [password,setPassword]=useState()
 let [user,setUser]=useState()
 
@@ -21,7 +22,7 @@ const handlePassword = function(e){
 
 useEffect(()=>{
     let obj ={
-        email:email,
+        mail:mail,
         password:password,
         form:'form',
         role:'user'
@@ -29,14 +30,38 @@ useEffect(()=>{
 
     setUser(obj)
 
-    },[email,password])
+    },[mail,password])
 
 const [signInUser] = useAddUserSignInMutation()
 
 const handleSubmit = function(e){
     e.preventDefault()
+    if(user.email.includes('@')===false){
+        Swal.fire({
+            title:'Error email',
+            text:'need include the @ sign',
+            confirmButtonText:'Write Again'
+        })
+    }else if(user.password.length < 4 ){
+        Swal.fire({
+            title:'Error Password',
+            text:'need be more of 4 characters',
+            confirmButtonText:'Write Again'
 
-    signInUser(user)
+        })
+
+    }else{
+        
+        signInUser(user)
+
+       Swal.fire({
+        icon:'success',
+        title:'Sign In successfully',
+        text : 'please look and read or create yours itineraries',
+        confirmButtonText:'Do It'
+       })
+    }
+
     
 }
 
@@ -47,11 +72,11 @@ const handleSubmit = function(e){
     <div className='Sign-container'>
         <h1 className='hSign'>Please Sign In</h1>
       <form onSubmit={handleSubmit} className="main-Sign">
-    <div className='form-Sign'>
+    <div className='form-SignIn'>
         <div className='inputs-p'>
 
     <p className='select-Sign'>Email</p>
-    <input type='email' onChange={handleEmail}></input>
+    <input type='text' onChange={handleEmail}></input>
     <p className='select-Sign'>Password</p>
     <input type='password' onChange={handlePassword}></input>
         </div>
