72fabc7f170630cd1a9435bf3a6407d4eb61f2b9
Facu00H <faisher00@gmail.com>
2022-09-23 02:02:53 -0300

    create page

---
+++ src/pages/CreateNewAdmin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import WebsiteLayout from '../layouts/WebsiteLayout'
+import CreateAdmin from '../components/CreateAdmin'
+import '../styles/DetailsPage.css'
+
+export default function CreateNewAdmin() {
+
+  return (
+    <WebsiteLayout>
+      <CreateAdmin />
+    </WebsiteLayout>
+  )
+}
