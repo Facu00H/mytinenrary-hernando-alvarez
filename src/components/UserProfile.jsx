56459e01800b1416bfe44d169de7fd83cdff3feb
Facu00H <faisher00@gmail.com>
2022-09-23 02:09:02 -0300

    Create card to contain user data

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/userProfile.css'
+import { useSelector } from 'react-redux'
+
+export default function UserProfile() {
+
+  
+  const user = useSelector(state => state.auth.user)
+  console.log(user)
+
+  return (
+    <div className="container">
+      <div className="card-profile">
+      <div className="imgProfile-container">
+        <img className="imgProfile" src={user.photo} alt={user.photo}/>
+      </div>
+      <div className="profile-data">
+        <h3>{user.name}</h3>
+        <p>{user.mail}</p>
+      </div>
+      </div>
+    </div>
+  )
+}
