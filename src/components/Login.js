c43b624328681fe92715955a218c35ee453055bd
Facu00H <faisher00@gmail.com>
2022-09-23 02:10:13 -0300

    use redux to modify interface

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,49 +2,52 @@ import { useState, useEffect } from 'react'
 import '../styles/Header.css'
 import { Link as LinkRouter } from 'react-router-dom'
 import { useAddUserSignOutMutation } from '../features/citiesAPI'
+import * as jose from 'jose'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { deleteCredentials } from '../features/authSlice'
+import { useNavigate } from "react-router-dom";
+
 
 
 
 
 
 export default function Login() {
-
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.auth.user)
+    const role = useSelector(state => state.auth.role)
     const [signOutUser] = useAddUserSignOutMutation()
-    const [role, setRole] = useState('')
+    const navigate = useNavigate();
 
-    useEffect(() => {
-        const userSession = JSON.parse(localStorage.getItem('user'))
-        setRole(userSession)
-    }, [])
 
-    function logOut() {
-        localStorage.removeItem('user')
-        localStorage.setItem('user',  JSON.stringify({
-            name: 'guest',
-            lastName:'-',
-            photo:'https://icon-library.com/images/generic-user-icon/generic-user-icon-19.jpg',
-            mail: '-',
-            role: 'guest'
-        }));
-        signOutUser({mail: role.mail})
-        setRole(JSON.parse(localStorage.getItem('user')))
+    async function logOut() {
+        try {
+            await signOutUser()
+            localStorage.removeItem('token')
+            dispatch(deleteCredentials())
+            navigate("/",{replace:true})
+        } catch(error) {
+            console.log(error)
+        }
     }    
 
     
 
     return (
         <div className="Login">
-            <img className='userImg' alt='user-img' src={role.photo}></img>
+            <img className='userImg' alt='user-img' src={user === null ? 'https://icon-library.com/images/generic-user-icon/generic-user-icon-19.jpg' : user.photo}></img>
             <div className='desplegable'> 
-                { role.role === 'guest' ?  
+                { role === null ?  
                 <div className='login-container'>
                     <LinkRouter to={"/auth/signin"} className='loginLink'>Sign In</LinkRouter>
                     <LinkRouter to={'/auth/signup'} className='loginLink'>Sign Up</LinkRouter>
                 </div> 
                     : 
                 <div className='login-container'>
-                    <LinkRouter to={"/auth/signin"} className='loginLink'>{role.name}</LinkRouter>
-                    <div className='loginLink' onClick={ logOut }>Log out</div>
+                    <LinkRouter to={"/Profile"} className='loginLink'>{user.name}</LinkRouter>
+                    {role === 'admin' ? <LinkRouter to={'/createAdmin'} className='loginLink'>Create admin</LinkRouter> : <></>}
+                    <LinkRouter to={"/cities"} className='loginLink' onClick={ logOut }>Log out</LinkRouter>
                 </div>    
                 }
             </div>
