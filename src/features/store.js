80d5fcca84f41a1439ce598d24745d6f8a160a3b
Facu00H <faisher00@gmail.com>
2022-09-23 02:06:50 -0300

    add authSlice in store

--- src/features/store.js
+++ src/features/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import citiesAPI from './citiesAPI'
+import authReducer from './authSlice'
 
 export default configureStore({
   reducer: {
-    [citiesAPI.reducerPath] : citiesAPI.reducer
+    [citiesAPI.reducerPath] : citiesAPI.reducer,
+    auth: authReducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
